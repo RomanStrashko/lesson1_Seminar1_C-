073b3737cd894064b5781d794d4b30d752d08b48
Roman <roman_strashko.8@mail.ru>
2022-09-16 23:38:36 +0300

    decision3

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка)
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} четное");
+}
+else
+{
+    Console.WriteLine($"Число {number} не четное");
+}
+
