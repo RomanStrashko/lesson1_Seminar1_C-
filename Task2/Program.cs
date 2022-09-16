b3c151cea020e62d627da796ab0b32e97c303013
Roman <roman_strashko.8@mail.ru>
2022-09-16 22:15:29 +0300

    decision1

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая на вход принимает два числа и выдает, какое чило больше, а какое меньше
+Console.Write("Введите первое число ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number < number2)
+{
+    Console.WriteLine($"Число {number} меньше числа {number2}");
+}
+if(number > number2)
+{
+    Console.WriteLine($"Число {number} больше числа {number2}");
+}
